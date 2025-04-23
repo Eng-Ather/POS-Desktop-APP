4e13271df7029c6973d5df260bf7d486d22846c3
Arsalan Ali <159891871+MrArsalAli@users.noreply.github.com>
2025-04-23 06:27:11 -0700

    connecting frontend to backend

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
+import { ipcRenderer } from "electron";
+
 const signInBtn = document.getElementById("signinBtn");
-const signinform = document.getElementById("signInForm")
+const signinform = document.getElementById("signInForm");
 const mail = document.querySelector("#email");
 const pass = document.querySelector("#password");
 
-// signInBtn.addEventListener("click", function (){
-//   console.log("THis function is working")
-//   console.log("email==>", mail.value)
-//   console.log("password==>", pass.value)
-//   window.location.href = "./screens/salesmanDashboard/index.html"
-// });
+signinform.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-signinform.addEventListener("submit", (e) => {
-  e.preventDefault()
-    alert("Kaam Kar raha he!")
+  const email = mail.value;
+  const password = pass.value;
 
+  try {
+    const response = await fetch("http://localhost:5000/user/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
-    window.location.href = "./screens/salesmanDashboard/index.html"
-})
+    const data = await response.json();
+    if (data.success) {
+      alert("Login successful!");
+    } else {
+      alert(data.message, "else");
+    }
+  } catch (err) {
+    alert("Login failed!");
+  }
+
+  // alert("Kaam Kar raha he!");
+
+  // window.location.href = "./screens/salesmanDashboard/index.html";
+});
